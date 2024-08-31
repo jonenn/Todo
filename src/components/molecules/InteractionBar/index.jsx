1c518334b73fbd46bde88d35d68b5bb09faa58ac
Juan J Malaver <juanmagri@outlook.es>
2024-08-30 19:19:39 -0500

    Searchbar layout setup

--- src/components/molecules/InteractionBar/index.jsx
+++ src/components/molecules/InteractionBar/index.jsx
@@ -2,6 +2,7 @@ import { InputBar } from '../../atoms/InputBar';
 import searchIcon from '../../../assets/search.svg';
 import createIcon from '../../../assets/plus.svg';
 import confirmIcon from '../../../assets/check2.svg';
+import './InteractionBar.css';
 
 const InteractionBar = ({ placeholder, type }) => {
    const barTypes = [
@@ -21,10 +22,10 @@ const InteractionBar = ({ placeholder, type }) => {
 
    const icon = barTypes.find((item) => item.type === type);
    return (
-      <>
+      <div className="interaction-bar">
          <InputBar placeholder={placeholder} />
          {icon && icon.icon}
-      </>
+      </div>
    );
 };
 
